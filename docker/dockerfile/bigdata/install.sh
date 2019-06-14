456dcd8c13e40352698184ec0833bb6621fc56ad
wangnz <wangnz@qq.com>
2019-06-14 15:21:03 +0800

    upd

---
+++ docker/dockerfile/bigdata/install.sh
@@ -0,0 +1,4 @@
+source /opt/install/install_jdk.sh
+source /opt/install/install_mvn.sh
+source /opt/install/install_scala.sh
+source /etc/bashrc
---
+++ docker/dockerfile/bigdata/install_mvn.sh
@@ -0,0 +1,6 @@
+tar -zxvf /opt/soft/apache-maven-3.6.1-bin.tar.gz -C /opt/soft/
+ln -s /opt/soft/apache-maven-3.6.1 /usr/local/maven
+echo 'export M2_HOME=/usr/local/maven' >> /etc/bashrc
+echo 'export PATH=$M2_HOME/bin:$PATH' >> /etc/bashrc
+source /etc/bashrc
+mvn -version
