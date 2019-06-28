df08ec654ee81138736785d60eea3e7c41755431
wangnz <wangnz@qq.com>
2019-06-28 09:54:04 +0800

    upd

--- docker/dockerfile/bigdata/hadoop/install_hadoop.sh
+++ docker/dockerfile/bigdata/hadoop/install_hadoop.sh
@@ -5,6 +5,16 @@ echo 'export PATH=$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$PATH' >> /etc/bashrc
 
 mkdir -p /opt/data/hadoop/hd1/data/tmp
 
+CONF_SRC=/opt/install
+CONF_HOME=/opt/soft/hadoop-3.1.2/etc/hadoop
+
+\cp $CONF_SRC/core-site.xml $CONF_HOME/core-site.xml
+\cp $CONF_SRC/hdfs-site.xml $CONF_HOME/hdfs-site.xml
+\cp $CONF_SRC/mapred-site.xml $CONF_HOME/mapred-site.xml
+\cp $CONF_SRC/yarn-site.xml $CONF_HOME/yarn-site.xml
+\cp $CONF_SRC/mapred-env.sh $CONF_HOME/mapred-env.sh
+\cp $CONF_SRC/hadoop-env.sh $CONF_HOME/hadoop-env.sh
+
 #hdfs namenode -format
 #hdfs --daemon start namenode
 #hdfs --daemon stop namenode
