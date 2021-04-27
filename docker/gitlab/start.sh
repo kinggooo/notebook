3f3ad2f366d9f5b96ce135c0894a0475df346535
wangnz <5323700@qq.com>
2021-04-27 10:23:15 +0800

    upd1

--- docker/gitlab/start.sh
+++ docker/gitlab/start.sh
@@ -6,10 +6,13 @@ CONTAINER_NAME=gitlab
 #docker rm $CONTAINER_NAME
 docker run -d \
   --hostname $HOST_NAME \
-  -p 8443:443 -p 10080:80 -p 2222:22 \
+  -p 8443:443 -p 10080:80 -p 10022:22 \
   --name $CONTAINER_NAME \
   --restart always \
+  --privileged \
   -v $GITLAB_DIR/config:/etc/gitlab \
   -v $GITLAB_DIR/logs:/var/log/gitlab \
   -v $GITLAB_DIR/data:/var/opt/gitlab \
-  gitlab/gitlab-ee:latest
+  gitlab-ce:latest
+
+#docker run -d -p 8443:443 -p 10080:80 -p 10022:22 --name gitlab centos:7 /usr/sbin/init
